ad124d026f80c2f82f75a635aa0836a59388660e
Paulo Bernardo <pbaf@ic.ufal.br>
2021-11-22 11:30:40 -0300

    feat: alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-snackbar
+      :value="alertState"
+      @input="setAlertState"
+      :color="alertColor"
+      :timeout="alertTimeout"
+    >
+      {{ alertMessage }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn text v-bind="attrs" @click="setAlertState(false)"> Close </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+  import { mapGetters, mapActions } from 'vuex';
+
+  export default {
+    name: 'Alert',
+    computed: {
+      ...mapGetters([
+        'alertState',
+        'alertMessage',
+        'alertColor',
+        'alertTimeout',
+      ]),
+    },
+    methods: {
+      ...mapActions(['setAlertState']),
+    },
+  };
+</script>
+
+<style></style>
