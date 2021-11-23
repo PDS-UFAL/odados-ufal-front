a715ce40facf1408d127f314bbef5b88669b4a45
Paulo Bernardo <pbaf@ic.ufal.br>
2021-11-23 09:24:54 -0300

    fix: missing bearer prefix

--- src/api/request.js
+++ src/api/request.js
@@ -7,7 +7,7 @@ export default {
       baseURL: process.env.VUE_APP_API_BASE_URL,
       headers: {
         ...(store.getters.authenticated
-          ? { Authorization: `${store.getters.authToken}` }
+          ? { Authorization: `Bearer ${store.getters.authToken}` }
           : {}),
       },
     });
