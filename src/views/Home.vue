e8a35f24423d2ce00706dcacbc1a74246c6c0359
Ricardo Alves <ras@ic.ufal.br>
2021-11-25 03:52:18 -0300

    refactor: input information and variable names

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,5 @@
 <template>
-  <div class="home">
-    <v-row class="d-flex justify-end align-center px-6 mt-3">
-      <v-btn x-large fab dark color="white" elevation="0">
-        <v-icon color="cloudy-grey"> mdi-account-circle </v-icon>
-      </v-btn>
-    </v-row>
-    <v-row class="mx-6">
-      <v-col cols="12" sm="6" md="4">
-        <v-card elevation="0" class="px-2" style="border: 2px solid #d9d016">
-          <v-row>
+  <v-container>
             <v-col class="d-flex justify-start" cols="8">
               <v-card-title> Abertos </v-card-title>
             </v-col>
@@ -29,83 +20,76 @@
       <v-col cols="12" sm="6" md="4">
         <v-card elevation="0" class="px-2" style="border: 2px solid #d91636">
           <v-row>
-            <v-col class="d-flex justify-start" cols="8">
-              <v-card-title> Não iniciados </v-card-title>
-            </v-col>
-            <v-col class="d-flex justify-end">
-              <v-checkbox
-                v-model="ex4"
-                color="red"
-                value="red"
-                hide-details
-              ></v-checkbox>
-            </v-col>
-          </v-row>
-          <v-card-subtitle class="d-flex justify-start">
-            <h1>3</h1>
-          </v-card-subtitle>
-        </v-card>
+
+    <v-row class="my-8">
+      <v-col cols="12" xs="12" md="4">
+        <v-text-field
+          name="search"
+          label="Pesquisar"
+          prepend-inner-icon="mdi-magnify"
+          outlined
+          dense
+          clearable
+          hide-details
+        />
       </v-col>
-      <v-col cols="12" sm="6" md="4">
-        <v-card elevation="0" class="px-2" style="border: 2px solid #6ad90b">
-          <v-row>
-            <v-col class="d-flex justify-start" cols="8">
-              <v-card-title> Finalizados </v-card-title>
-            </v-col>
-            <v-col class="d-flex justify-end">
-              <v-checkbox
-                v-model="ex4"
-                color="green"
-                value="green"
-                hide-details
-              ></v-checkbox>
-            </v-col>
-          </v-row>
-          <v-card-subtitle class="d-flex justify-start">
-            <h1>10</h1>
-          </v-card-subtitle>
-        </v-card>
+
+      <v-col cols="12" xs="12" md="2" class="py-0 mt-md-3 pr-md-0">
+        <span class="d-flex justify-md-end align-center mt-md-2">
+          Ordenar por:
+        </span>
       </v-col>
-    </v-row>
-    <v-row class="mx-6">
-      <v-col cols="12" md="4" sm="4">
-        <v-text-field label="Pesquisar" outlined dense></v-text-field>
+
+      <v-col cols="12" xs="12" md="3" class="pt-0 pt-md-3">
+        <v-select
+          v-model="sortBy"
+          :items="sortableItems"
+          :menu-props="{ 'offset-y': true }"
+          dense
+          outlined
+          hide-details
+        />
       </v-col>
-      <v-col cols="4" sm="3" md="2">
-        <span class="d-flex justify-end align-center mt-2">Ordenar por:</span>
-      </v-col>
-      <v-col class="d-flex" cols="8" sm="8" md="3">
-        <v-select :items="items" dense outlined clearable></v-select>
-      </v-col>
-      <v-col cols="12" sm="6" md="3">
+
+      <v-col cols="12" xs="12" md="3">
         <v-menu
-          v-model="menu2"
+          v-model="showDatepicker"
           :close-on-content-click="false"
-          :nudge-right="40"
+          :nudge-right="0"
           transition="scale-transition"
+          max-width="290px"
+          min-width="290px"
           offset-y
-          min-width="auto"
         >
           <template v-slot:activator="{ on, attrs }">
             <v-text-field
-              v-model="dateFormatted"
-              prepend-icon="mdi-calendar"
-              label="Data"
-              readonly
-              v-bind="attrs"
               v-on="on"
-              outlined
+              v-bind="attrs"
+              label="Criado em"
+              :value="dateRangeText"
+              prepend-inner-icon="mdi-calendar"
               dense
-            ></v-text-field>
+              readonly
+              outlined
+              clearable
+              hide-details
+              @click:clear="selectedDates = []"
+              @click:prepend-inner="showDatepicker = true"
+            />
           </template>
           <v-date-picker
-            v-model="date"
-            @input="menu2 = false"
+            color="primary"
+            v-model="selectedDates"
             locale="pt-br"
-          ></v-date-picker>
+            range
+            scrollable
+            @change="setDates"
+            :title-date-format="dateTitle"
+          />
         </v-menu>
       </v-col>
     </v-row>
+
     <v-row class="mx-8">
       <v-expansion-panels>
         <v-expansion-panel v-for="(item, i) in 5" :key="i">
@@ -127,10 +111,12 @@
         </v-expansion-panel>
       </v-expansion-panels>
     </v-row>
-  </div>
+  </v-container>
 </template>
 
 <script>
+  import { formatDate } from '@/utils/formatDate';
+
   export default {
     name: 'Home',
     data: (vm) => {
@@ -143,6 +129,12 @@
           'Pendentes',
           'Data de fim',
         ],
+        sortableItems: [
+          { text: 'Nome', value: 'name' },
+          { text: 'Data de início', value: 'initial_date' },
+          { text: 'Data de fim', value: 'end_date' },
+        ],
+
         date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
           .toISOString()
           .substr(0, 10),
@@ -162,12 +154,32 @@
       },
     },
 
+        sortBy: 'name',
+        showDatepicker: false,
+        selectedDates: [],
+      };
+    },
     methods: {
+      setDates(dates) {
+        this.selectedDates = dates.sort();
+      },
+      dateTitle() {
+        return this.selectedDates.length === 2
+          ? 'Intervalo selecionado'
+          : formatDate(this.selectedDates[0]) || '-';
+      },
       formatDate(date) {
         if (!date) return null;
 
         const [year, month, day] = date.split('-');
         return `${day}/${month}/${year}`;
+    },
+    computed: {
+      dateRangeText() {
+        const formattedDates = this.selectedDates.map((date) =>
+          formatDate(date),
+        );
+        return formattedDates.join(' à ');
       },
     },
   };
