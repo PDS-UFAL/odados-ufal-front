0acc020b04b1c5d0aaabef449b1f848250695a46
Ricardo Alves <ras@ic.ufal.br>
2021-12-01 20:07:10 -0300

    feat(form): adds question component

--- src/views/CreateForms.vue
+++ src/views/CreateForms.vue
@@ -7,20 +7,21 @@
       <h3>Novo formulário</h3>
     </v-row>
 
-    <v-row class="my-8 mx-2">
-      <v-col cols="12" xs="12" md="4" class="pa-0">
+    <v-row>
+      <v-col cols="12" md="6" class="pa-0">
         <v-text-field
-          name="search"
-          label="Pesquisar"
-          prepend-inner-icon="mdi-magnify"
-          outlined
+          v-model="title"
+          name="title"
+          label="Título do formulário"
           dense
+          outlined
           clearable
         />
       </v-col>
+
       <v-spacer />
 
-      <v-col cols="12" xs="12" md="3" class="pa-0 mr-4">
+      <v-col cols="12" md="4" class="pa-0 mr-4">
         <v-menu
           v-model="showDatepicker"
           :close-on-content-click="false"
@@ -37,9 +38,9 @@
               label="Data"
               :value="dateRangeText"
               prepend-inner-icon="mdi-calendar"
+              dense
               readonly
               outlined
-              dense
               clearable
               @click:clear="dates = []"
               @click:prepend-inner="showDatepicker = true"
@@ -58,22 +59,24 @@
       </v-col>
     </v-row>
 
-    <v-row class="my-4 mx-2">
+    <v-row class="my-8">
       <h3>Quem vai responder?</h3>
     </v-row>
 
     <v-row>
-      <v-card elevation="3" class="px-2 mt-4 ma-3" width="100%">
+      <v-card elevation="3" class="px-2" width="100%">
         <v-col class="d-flex align-center justify-end">
           <v-btn
             @click="checkAll"
             :disabled="selected.length == sectors.length"
             text
-            >Marcar todos</v-btn
+            small
           >
-          <v-btn :disabled="selected.length == 0" text bottom @click="undo"
-            >Desmarcar todos</v-btn
-          >
+            Marcar todos
+          </v-btn>
+          <v-btn :disabled="selected.length == 0" text small @click="undo">
+            Desmarcar todos
+          </v-btn>
         </v-col>
 
         <v-row class="mx-4">
@@ -83,21 +86,48 @@
                 v-model="selected"
                 :value="sector.name"
                 :label="sector.name"
-              ></v-checkbox>
+              />
             </v-col>
           </div>
         </v-row>
       </v-card>
     </v-row>
 
-    <v-row class="pt-6 mx-2">
+    <v-row class="mt-8">
       <h3>Perguntas</h3>
-      <v-spacer></v-spacer>
+      <v-spacer />
       <v-btn text color="primary">
         <v-icon>mdi-plus</v-icon>
         Importar perguntas
       </v-btn>
     </v-row>
+
+    <v-layout row class="py-4" justify-center>
+      <question-card
+        v-for="(question, index) in questions"
+        :key="index"
+        class="my-4"
+        :question="question"
+        @duplicateQuestion="duplicateQuestion"
+        @removeQuestion="removeQuestion"
+      />
+
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-bind="attrs"
+            v-on="on"
+            fab
+            small
+            color="primary"
+            @click="addQuestion"
+          >
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </template>
+        <span>Adicionar pergunta</span>
+      </v-tooltip>
+    </v-layout>
 
     <div class="add-btn">
       <v-tooltip left>
@@ -114,9 +144,13 @@
 
 <script>
   import { formatDate } from '@/utils/formatDate';
+  import QuestionCard from '@/components/QuestionCard';
 
   export default {
     name: 'CreateForms',
+    components: {
+      QuestionCard,
+    },
     data: () => {
       return {
         sectors: [
@@ -145,6 +179,9 @@
         selected: [],
         dates: [],
         showDatepicker: false,
+
+        title: null,
+        questions: [{ id: 0 }],
       };
     },
     methods: {
@@ -164,6 +201,20 @@
         return this.dates.length === 2
           ? 'Intervalo selecionado'
           : formatDate(this.dates[0]) || '-';
+      },
+      addQuestion() {
+        this.questions.push({ id: this.questions.length });
+      },
+      duplicateQuestion(question) {
+        console.log(question);
+        this.questions.push({ ...question, id: this.questions.length });
+      },
+      removeQuestion(id) {
+        if (this.questions.length === 1) return;
+
+        this.questions = [
+          ...this.questions.filter((question) => question.id !== id),
+        ];
       },
     },
     computed: {
