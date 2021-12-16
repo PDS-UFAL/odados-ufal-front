03e1fac9fc2f3ee4049195008640a9a8ea39c2fb
Ricardo Alves <ras@ic.ufal.br>
2021-12-16 05:36:29 -0300

    feat: component for each input type

---
+++ src/components/form/answers/Number.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row>
+    <v-col>
+      <span class="text-subtitle-1 font-weight-bold">
+        {{ question.title }}
+      </span>
+
+      <v-text-field
+        v-model="question.example"
+        type="number"
+        :min="question.min_value"
+        :max="question.max_value"
+        :rules="[rules.required, ...rules.number]"
+        dense
+        outlined
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  import rules from '@/mixins/rules';
+
+  export default {
+    name: 'Number',
+    props: {
+      question: {
+        type: Object,
+        required: true,
+      },
+    },
+    mixins: [rules],
+  };
+</script>
+
+<style></style>
