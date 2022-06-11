03bcf492ae2f30fa7500ee412e2761100a70806e
PeterMatthew <pedro_mateus_12@hotmail.com>
2022-06-10 21:45:19 -0300

    add form title in sector answer page

--- src/views/AnswerForm.vue
+++ src/views/AnswerForm.vue
@@ -11,12 +11,17 @@
       <v-btn @click="back" text fab small class="mr-2">
         <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
-      <h3>{{ form.subtitle }}</h3>
+      <h3>{{ form.form.title }}</h3>
 
       <v-spacer />
 
       <span class="font-weight-light">{{ relativeTime }}</span>
     </v-row>
+
+    <h4 style="margin-top: -2rem; margin-bottom: 2rem; color: #6b7280">
+      {{ form.subtitle }}
+    </h4>
+
     <h4 style="color: #34d399; margin-bottom: 1rem" v-if="hasResponse">
       Você já respondeu este formulário
     </h4>
